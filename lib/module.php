2ea72e25a6ee50ce3afea059dbce4a7c4fe1cb36
ddenis <dhffdh@gmail.com>
2017-07-06 20:29:38 +0800

    #7166: camelCase и новый класс

--- lib/module.php
+++ lib/module.php
@@ -8,11 +8,7 @@
 
 namespace Pwd\Offerchanger;
 
-use \Bitrix\Main\Context,
-    \Pwd\Offerchanger\Utils as Utils,
-    \Pwd\Offerchanger\Hl as HL,
-    \Bitrix\Main\Config\Option,
-    \Bitrix\Main\EventManager;
+use \Bitrix\Main\EventManager;
 
 /**
  * Основной класс модуля
@@ -20,8 +16,7 @@ use \Bitrix\Main\Context,
 class Module
 {
 
-    protected static $hl_code = 'OfferChanger';
-    protected static $MODULE_ID = 'pwd.offerchanger';
+
 
     /**
      * Обработчик начала отображения страницы
@@ -43,14 +38,14 @@ class Module
      */
     public static function setupEventHandlers()
     {
-        $eventManager = EventManager::getInstance();
+        $objEventManager = EventManager::getInstance();
 
-        $eventManager->addEventHandler(
+        $objEventManager->addEventHandler(
             "",
             "OfferChangerOnAfterUpdate",
             array('\Pwd\Offerchanger\Hl', 'HeadersOnAfterUpdateHandler')
         );
-        $eventManager->addEventHandler(
+        $objEventManager->addEventHandler(
             "",
             "OfferChangerOnAfterAdd",
             array('\Pwd\Offerchanger\Hl', 'HeadersOnAfterUpdateHandler')
@@ -58,155 +53,13 @@ class Module
 
         if( ! \CSite::InDir('/bitrix/') ){
 
-            $eventManager->addEventHandler(
+            $objEventManager->addEventHandler(
                 "main",
                 "OnEndBufferContent",
-                array('\Pwd\Offerchanger\Module', 'onEndBufferContentHandler')
+                array('\Pwd\Offerchanger\Offer', 'onEndBufferContentHandler')
             );
 
         }
 
-
-
     }
-
-    /**
-     * Обработчик вызывается при выводе буферизированного контента.
-     *
-     * @param $content
-     */
-    public static function onEndBufferContentHandler(&$content)
-    {
-
-        self::setOffers($content);
-
-    }
-
-    /**
-     * Устаналвиет в контент блоки замен
-     *
-     * @param $content
-     * @return bool
-     */
-    public static function setOffers(&$content)
-    {
-
-        // Проверяет настроку модуля(Только на индексной странице?)
-        $only_index = (Option::get(self::$MODULE_ID, 'only_index') == 'Y') ? true : false;
-        if ($only_index && !\CSite::InDir('/index.php')) {
-            return false;
-        }
-
-        $doc = new \DOMDocument();
-        $doc->loadHTML($content);
-
-        $r_list = self::getOffers();
-        if (!empty($r_list) && count($r_list)) {
-            foreach ($r_list as $r_item) {
-
-
-
-                if(empty($r_item['ID'])){
-                    continue;
-                }
-
-                $dom_element = $doc->getElementById($r_item['ID']);
-
-                if( empty($dom_element) || !is_object( $dom_element )){
-                    continue;
-                }
-
-
-                switch ($r_item['TYPE']){
-                    case 'BG':
-                        //background-image:
-
-                        if (!empty($r_item['BANNER']) && strlen($r_item['BANNER'])) {
-
-                            $new_styles = 'background-image: url("'.$r_item['BANNER'].'");';
-
-                            $old_styles = $dom_element->getAttribute('style');
-                            $dom_element->setAttribute( 'style',$old_styles." ".$new_styles );
-
-                        }
-
-                        break;
-                    case 'IMG':
-                        //Изображения
-                        if (!empty($r_item['BANNER']) && strlen($r_item['BANNER'])) {
-
-                            $new_img = $r_item['BANNER'];
-
-                            $old_img = $dom_element->getAttribute('src');
-                            $dom_element->setAttribute( 'src', $new_img);
-                            $dom_element->setAttribute( 'data-old-src', $old_img);
-
-                        }
-                        break;
-                    case 'STRING':
-                    default:
-                        //строка
-
-                        if (!empty($r_item['TEXT'])) {
-                            $dom_element->textContent = $r_item['TEXT'];
-                        }
-
-                    break;
-                }
-            }
-        }
-
-        $content = $doc->saveHTML();
-
-        return true;
-    }
-
-    /**
-     * Получает блоки замен из HL-блоков
-     *
-     * @return array
-     */
-    public static function getOffers()
-    {
-
-        $referrer_name = Option::get(self::$MODULE_ID, 'referrer_name');
-        $referrer_name = $referrer_name !== '' ? $referrer_name : 'referrer';
-
-        $arHeaders = HL::getInstance(self::$hl_code)->getHeaders($referrer_name);
-
-        $res_list = [];
-        foreach ($arHeaders as $arHeader) {
-
-            if (!empty($arHeader)) {
-                $res = array();
-
-
-                if($arHeader['UF_TYPE']>0){
-                    $rsGender = \CUserFieldEnum::GetList(array(), array("ID" => $arHeader['UF_TYPE']));
-
-                    if($arGender = $rsGender->GetNext()){
-                        $res['TYPE'] = $arGender['XML_ID'];
-                    }
-
-                }else{
-                    $res['TYPE'] = 'STRING';
-                }
-
-
-                $res['ID'] = $arHeader['UF_BLOCK_ID'];
-                $res['TEXT'] = $arHeader['UF_BLOCK_TEXT'];
-
-                $res['BANNER_TMP'] = \CFile::GetFileArray($arHeader['UF_BANNER']);
-                if (!empty($res['BANNER_TMP'])) {
-                    $res['BANNER'] = $res['BANNER_TMP']['SRC'];
-                }
-                unset($res['BANNER_TMP']);
-
-                $res_list[] = $res;
-            }
-        }
-        return $res_list;
-    }
-
-
 }
---
+++ lib/offer.php
@@ -0,0 +1,162 @@
+<?
+/**
+ * Created by PhpStorm
+ * User: Uriy Smirnov & Denis Denisov
+ * p-w-d.ru
+ * @ Pride Web Development
+ */
+
+namespace Pwd\Offerchanger;
+
+use\Pwd\Offerchanger\Hl as HL,
+    \Bitrix\Main\Config\Option;
+
+/**
+ * Основной класс модуля
+ */
+class Offer
+{
+
+
+    protected static $strHlCode = 'OfferChanger';
+    protected static $strModuleId = 'pwd.offerchanger';
+
+    /**
+     * Обработчик вызывается при выводе буферизированного контента.
+     *
+     * @param $content
+     */
+    public static function onEndBufferContentHandler(&$content)
+    {
+
+        self::setOffers($content);
+
+    }
+
+    /**
+     * Устаналвиет в контент блоки замен
+     *
+     * @param $content
+     * @return bool
+     */
+    public static function setOffers(&$content)
+    {
+
+        // Проверяет настроку модуля(Только на индексной странице?)
+        $boolOnlyIndex = (Option::get(self::$strModuleId, 'only_index') == 'Y') ? true : false;
+        if ($boolOnlyIndex && !\CSite::InDir('/index.php')) {
+            return false;
+        }
+
+        $objDocument = new \DOMDocument();
+        $objDocument->loadHTML($content);
+
+        $arListOffers = self::getOffers();
+        if (!empty($arListOffers) && count($arListOffers)) {
+
+            foreach ($arListOffers as $arOfferItem) {
+
+
+
+                if(empty($arOfferItem['ID'])){
+                    continue;
+                }
+
+                $objDomElement = $objDocument->getElementById($arOfferItem['ID']);
+
+                if( empty($objDomElement) || !is_object( $objDomElement )){
+                    continue;
+                }
+
+
+                switch ($arOfferItem['TYPE']){
+                    case 'BG':
+                        //background-image:
+
+                        if (!empty($arOfferItem['BANNER']) && strlen($arOfferItem['BANNER'])) {
+
+                            $strNewStyles = 'background-image: url("'.$arOfferItem['BANNER'].'");';
+                            $strOldStyles = $objDomElement->getAttribute('style');
+
+                            $objDomElement->setAttribute( 'style',$strOldStyles." ".$strNewStyles );
+
+                        }
+
+                        break;
+                    case 'IMG':
+                        //Изображения
+                        if (!empty($arOfferItem['BANNER']) && strlen($arOfferItem['BANNER'])) {
+
+                            $strNewImg = $arOfferItem['BANNER'];
+                            $strOldImg = $objDomElement->getAttribute('src');
+
+                            $objDomElement->setAttribute( 'src', $strNewImg);
+                            $objDomElement->setAttribute( 'data-old-src', $strOldImg);
+
+                        }
+                        break;
+                    case 'STRING':
+                    default:
+                        //строка
+
+                        if (!empty($arOfferItem['TEXT'])) {
+                            $objDomElement->textContent = $arOfferItem['TEXT'];
+                        }
+
+                        break;
+                }
+            }
+        }
+
+        $content = $objDocument->saveHTML();
+
+        return true;
+    }
+
+    /**
+     * Получает блоки замен из HL-блоков
+     *
+     * @return array
+     */
+    protected static function getOffers()
+    {
+
+        $strReferrerName = Option::get(self::$strModuleId, 'referrer_name');
+        $strReferrerName = $strReferrerName !== '' ? $strReferrerName : 'referrer';
+
+        $arHeaders = HL::getInstance(self::$strHlCode)->getHeaders($strReferrerName);
+
+        $arOffersList = [];
+        foreach ($arHeaders as $arHeader) {
+
+            if (!empty($arHeader)) {
+                $arResOffer = array();
+
+
+                if($arHeader['UF_TYPE']>0){
+                    $rsGender = \CUserFieldEnum::GetList(array(), array("ID" => $arHeader['UF_TYPE']));
+
+                    if($arGender = $rsGender->GetNext()){
+                        $arResOffer['TYPE'] = $arGender['XML_ID'];
+                    }
+
+                }else{
+                    $arResOffer['TYPE'] = 'STRING';
+                }
+
+
+                $arResOffer['ID'] = $arHeader['UF_BLOCK_ID'];
+                $arResOffer['TEXT'] = $arHeader['UF_BLOCK_TEXT'];
+
+                $arResOffer['BANNER_TMP'] = \CFile::GetFileArray($arHeader['UF_BANNER']);
+                if (!empty($arResOffer['BANNER_TMP'])) {
+                    $arResOffer['BANNER'] = $arResOffer['BANNER_TMP']['SRC'];
+                }
+                unset($arResOffer['BANNER_TMP']);
+
+                $arOffersList[] = $arResOffer;
+            }
+        }
+        return $arOffersList;
+    }
+}
